4c58f2a849a826b55f3494877a80bd01cc4550ad
Tanya Karpenko <wualyaarrr@gmail.com>
2022-11-17 11:56:50 +0300

    Initial Commit

---
+++ getirRedesign/getirRedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  getirRedesignApp.swift
+//  getirRedesign
+//
+//  Created by Tatiana Karpenko on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct getirRedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
